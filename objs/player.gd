d3ac4f782b24b939c7febfbcacf7ec9ba70e82c9
Ben Eng <whysguy52@gmail.com>
2023-09-29 22:48:20 -0500

    room updated

--- objs/player.gd
+++ objs/player.gd
@@ -23,44 +23,44 @@ func _unhandled_input(event):
 func movement(delta):
   # add the gravity
   if not is_on_floor():
-    velocity.y -= gravity * delta * FAKE_EXTRA_GRAVITY
+	velocity.y -= gravity * delta * FAKE_EXTRA_GRAVITY
 
   # wasd movement
   var input_dir = Input.get_vector("left", "right", "up", "down")
   var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 
   if direction:
-    velocity.x = direction.x * SPEED
-    velocity.z = direction.z * SPEED
+	velocity.x = direction.x * SPEED
+	velocity.z = direction.z * SPEED
   else:
-    velocity.x = move_toward(velocity.x, 0, SPEED)
-    velocity.z = move_toward(velocity.z, 0, SPEED)
+	velocity.x = move_toward(velocity.x, 0, SPEED)
+	velocity.z = move_toward(velocity.z, 0, SPEED)
 
   move_and_slide()
 
 
 func unhandled_input_actions(event : InputEvent):
   if event.is_action_pressed("action"):
-    Action.perform()
+	Action.perform()
 
   if event.is_action_pressed("test_module"):
-    if module:
-      switch_carry_module()
-    else:
-      add_carry_module(MODULE_TYPES.pick_random())
+	if module:
+	  switch_carry_module()
+	else:
+	  add_carry_module(MODULE_TYPES.pick_random())
 
 
 func update_carry_module_material(carry_module):
   var material = preload("res://assets/materials/default_module.tres")
 
   if module == "farm":
-    material = preload("res://assets/materials/farm.tres")
+	material = preload("res://assets/materials/farm.tres")
   elif module == "drill":
-    material = preload("res://assets/materials/drill.tres")
+	material = preload("res://assets/materials/drill.tres")
   elif module == "oxygen pump":
-    material = preload("res://assets/materials/oxygen_pump.tres")
+	material = preload("res://assets/materials/oxygen_pump.tres")
   elif module == "generator":
-    material = preload("res://assets/materials/generator.tres")
+	material = preload("res://assets/materials/generator.tres")
 
   carry_module.get_node('mesh').material_override = material
 
@@ -75,8 +75,8 @@ func add_carry_module(type):
 
 func remove_carry_module():
   for node in $carry_module_spawn.get_children():
-    $carry_module_spawn.remove_child(node)
-    node.queue_free()
+	$carry_module_spawn.remove_child(node)
+	node.queue_free()
 
   module = null
   Action.update_changes()
@@ -87,7 +87,7 @@ func switch_carry_module():
   var next_module_index = module_index + 1
 
   if next_module_index > MODULE_TYPES.size() - 1:
-    next_module_index = 0
+	next_module_index = 0
 
   module = MODULE_TYPES[next_module_index]
 
